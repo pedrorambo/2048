ace29bb466b9df0a59627b9048f13bc2b8708280
Pedro Rambo <prambo@protonmail.com>
2021-04-12 18:16:38 -0300

    Add get random boolean by change

--- main.c
+++ main.c
@@ -10,6 +10,11 @@ float getRandomNumber(int min, int max)
     return min + (rand() % (max - min + 1));
 }
 
+int getRandomBooleanByChance(int chance){
+    int randomNumber = getRandomNumber(1, 100);
+    return randomNumber <= chance;
+}
+
 int getRandomPosition()
 {
     int minValue = 0;
