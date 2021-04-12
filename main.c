56791c48bb3930c95c2342f7ab8b527685650185
Pedro Rambo <prambo@protonmail.com>
2021-04-12 16:51:53 -0300

    Add add value 2 to random position on table and printTable
    Add getRandomNumber;
    Add getRandomPosition;

--- main.c
+++ main.c
@@ -1,5 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#define TABLE_SIZE 4
 
-int main(){
+float getRandomNumber(int min, int max)
+{
+    int randomNumber = rand();
+    return min + (rand() % (max - min + 1));
+}
+
+int getRandomPosition()
+{
+    int minValue = 0;
+    int maxValue = (TABLE_SIZE * TABLE_SIZE) - 1;
+
+    return getRandomNumber(minValue, maxValue);
+}
+
+void addValue2ToRandomPositionOnTable(int table[TABLE_SIZE][TABLE_SIZE])
+{
+    int isOccupied = 0;
+    do
+    {
+        int randomPosition = getRandomPosition();
+        int l = floor(randomPosition / TABLE_SIZE);
+        int c = randomPosition % TABLE_SIZE;
+        isOccupied = table[l][c];
+        if(!isOccupied){
+            table[l][c] = 2;
+        }
+    } while (isOccupied);
+}
+
+void printTable(int table[TABLE_SIZE][TABLE_SIZE])
+{
+    for (int l = 0; l < TABLE_SIZE; l++)
+    {
+        for (int c = 0; c < TABLE_SIZE; c++)
+        {
+            printf("%3d ", table[l][c]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int main()
+{
+    srand(time(NULL));
+    int table[TABLE_SIZE][TABLE_SIZE] = {0};
+
+    addValue2ToRandomPositionOnTable(table);
+    printTable(table);
+    printf("\n\n");
+    
     printf("Hello, world!");
 }
