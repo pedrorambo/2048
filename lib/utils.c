f9c2072e1d2949df2e4d71d5b798e881731f28b5
Pedro Rambo <prambo@protonmail.com>
2021-04-14 16:19:41 -0300

    Update utils removed unused variable

--- lib/utils.c
+++ lib/utils.c
@@ -2,11 +2,11 @@
 
 float getRandomNumber(int min, int max)
 {
-    int randomNumber = rand();
     return min + (rand() % (max - min + 1));
 }
 
-int getRandomBooleanByChance(int chance){
+int getRandomBooleanByChance(int chance)
+{
     int randomNumber = getRandomNumber(1, 100);
     return randomNumber <= chance;
 }
