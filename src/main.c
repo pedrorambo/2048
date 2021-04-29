96efad49f914a907c565da8dfb0a7c7f297d6f3e
Pedro Rambo <prambo@protonmail.com>
2021-04-29 11:03:10 -0300

    Update main.c indentation
    Update tab to spaces

--- src/main.c
+++ src/main.c
@@ -8,40 +8,40 @@
 
 int main(void)
 {
-	srand(time(NULL));
-	int table[TABLE_SIZE][TABLE_SIZE] = {0};
-	initView();
-	addInitialPiecesToTable(table);
-	drawTable(table);
+    srand(time(NULL));
+    int table[TABLE_SIZE][TABLE_SIZE] = {0};
+    initView();
+    addInitialPiecesToTable(table);
+    drawTable(table);
 
-	while (TRUE)
-	{
-		int key = getNextKey();
+    while (TRUE)
+    {
+        int key = getNextKey();
 
-		switch (key)
-		{
-		case 66: // Down
-			if (playDown(table) != 0)
-				addPiecesToTable(table);
-			drawTable(table);
-			break;
-		case 65: // Up
-			if (playUp(table) != 0)
-				addPiecesToTable(table);
-			drawTable(table);
-			break;
-		case 68: // Left
-			if (playLeft(table) != 0)
-				addPiecesToTable(table);
-			drawTable(table);
-			break;
-		case 67: // Right
-			if (playRight(table) != 0)
-				addPiecesToTable(table);
-			drawTable(table);
-			break;
-		default:
-			break;
-		}
-	}
+        switch (key)
+        {
+        case 66: // Down
+            if (playDown(table) != 0)
+                addPiecesToTable(table);
+            drawTable(table);
+            break;
+        case 65: // Up
+            if (playUp(table) != 0)
+                addPiecesToTable(table);
+            drawTable(table);
+            break;
+        case 68: // Left
+            if (playLeft(table) != 0)
+                addPiecesToTable(table);
+            drawTable(table);
+            break;
+        case 67: // Right
+            if (playRight(table) != 0)
+                addPiecesToTable(table);
+            drawTable(table);
+            break;
+        default:
+            break;
+        }
+    }
 }
