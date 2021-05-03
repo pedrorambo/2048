f088ba4f363cbe94ea91028a388c57c97ec3ef29
Pedro Rambo <prambo@protonmail.com>
2021-05-03 08:50:52 -0300

    Add stats
    Add score and movements
    Add ranking
    Update colors

--- src/include/config.h
+++ src/include/config.h
@@ -2,5 +2,7 @@
 #define CONFIG_H
 
 #define TABLE_SIZE 4
+#define USERNAME_MAX_LENGTH 20
+#define RANKING_SIZE 10
 
 #endif
--- src/tableView.c
+++ src/tableView.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#include <string.h>
 #include <config.h>
 #include <tableLogic.h>
 #include <view.h>
 #include <tableView.h>
 #include <core.h>
+
+#define CARD_TITLE_PADDING 2
+#define CARD_VALUE_PADDING 1
+
+#define SCORE_CARD_WIDTH 9
+#define MOVEMENTS_CARD_WIDTH 14
+
+#define RANKING_CARD_WIDTH 24
+#define RANKING_CARD_SCORE_WIDTH 7
 
 void setColorByValue(WINDOW *window, int value)
 {
@@ -84,9 +94,52 @@ void drawTable(WINDOW *window, int table[TABLE_SIZE][TABLE_SIZE])
     }
 }
 
+void drawInt(WINDOW *window, int value, int x, int y, int width, int padding, int colorPairIndex)
+{
+    int valueMaxLength = width - (padding * 2);
+    wattron(window, COLOR_PAIR(colorPairIndex));
+    wmove(window, y, x);
+    wprintw(window, "%*c%*d%*c", padding, ' ', valueMaxLength, value, padding, ' ');
+}
+
+void drawString(WINDOW *window, char value[], int x, int y, int width, int padding, int colorPairIndex)
+{
+    int valueMaxLength = width - (padding * 2);
+    wattron(window, COLOR_PAIR(colorPairIndex));
+    wmove(window, y, x);
+    wprintw(window, "%*c%*s%*c", padding, ' ', valueMaxLength, value, padding, ' ');
+}
+
+void drawCardInt(WINDOW *window, char title[], int value, int x, int y, int width)
+{
+    drawString(window, title, x, y, width, CARD_TITLE_PADDING, VIEW_COLOR_LIGHT_GREY);
+    drawInt(window, value, x, y + 1, width, CARD_VALUE_PADDING, VIEW_COLOR_GREY);
+}
+
+void drawCardRanking(WINDOW *window, char title[], t_tableData *tableData, int x, int y, int width)
+{
+    drawString(window, title, x, y, width, CARD_TITLE_PADDING, VIEW_COLOR_LIGHT_GREY);
+
+    for (int i = 0; i < tableData->rankingSize; i++)
+    {
+        drawInt(window, tableData->ranking[i].score, x, y + i + 1, RANKING_CARD_SCORE_WIDTH, CARD_VALUE_PADDING, VIEW_COLOR_GREY);
+        drawString(window, tableData->ranking[i].name, x + RANKING_CARD_SCORE_WIDTH, y + i + 1, width - RANKING_CARD_SCORE_WIDTH, CARD_VALUE_PADDING, VIEW_COLOR_GREY);
+    }
+}
+
+void drawStats(WINDOW *window, t_tableData *tableData)
+{
+    int tableHorizontalSize = PIECE_WIDTH * TABLE_SIZE;
+
+    drawCardInt(window, "Score", tableData->score, tableHorizontalSize + 2, 1, SCORE_CARD_WIDTH);
+    drawCardInt(window, "Movimentos", tableData->movements, tableHorizontalSize + 2 + SCORE_CARD_WIDTH + 1, 1, MOVEMENTS_CARD_WIDTH);
+    drawCardRanking(window, "Ranking", tableData, tableHorizontalSize + 2, 4, RANKING_CARD_WIDTH);
+}
+
 void renderTable(WINDOW *window, t_tableData *tableData)
 {
     wclear(window);
     drawTable(window, tableData->table);
+    drawStats(window, tableData);
     wrefresh(window);
 }
