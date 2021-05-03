487735ec0e1f9683975c54ea24693dea6819411c
Lucas Chiloff <lucaschiloff@gmail.com>
2021-05-03 20:58:31 -0300

    Added documentation for core.h

--- src/include/core.h
+++ src/include/core.h
@@ -3,12 +3,14 @@
 #ifndef CORE_H
 #define CORE_H
 
+/*Estrutura para guardar informações do usuário*/
 typedef struct
 {
     int score;
     char name[USERNAME_MAX_LENGTH + 1];
 } t_user;
 
+/*Estrutura base para o jogo*/
 typedef struct
 {
     int table[TABLE_SIZE][TABLE_SIZE];
@@ -22,12 +24,19 @@ typedef struct
     int gameFinished;
 } t_tableData;
 
+/*Função para mover as peças para cima*/
 void playUp(t_tableData *, int);
+/*Função para mover as peças para baixo*/
 void playDown(t_tableData *, int);
+/*Função para mover as peças para esquerda*/
 void playLeft(t_tableData *, int);
+/*Função para mover as peças para direita*/
 void playRight(t_tableData *, int);
+/*Função para reinicializar os dados do jogo*/
 void flushData(t_tableData *tableData);
+/*Função para adicionar duas peças ao tabuleiro no ínicio do jogo*/
 void addInitialPieces(t_tableData *tableData);
+/*Função para limpar o nome do arquivo*/
 void clearFileName(t_tableData *tableData);
 
 #endif
