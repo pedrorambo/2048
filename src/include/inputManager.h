96708be960ee1b8d3571ef2462f162ea2c6fb682
Pedro Rambo <prambo@protonmail.com>
2021-05-03 21:02:56 -0300

    Add docs

--- src/include/inputManager.h
+++ src/include/inputManager.h
@@ -4,7 +4,10 @@
 #ifndef INPUT_MANAGER_H
 #define INPUT_MANAGER_H
 
+/* Gerencia a visualização da janela selecionada */
 void handleWindow(WINDOW *window, t_tableData *tableData, const unsigned int currentWindow);
+
+/* Gerenciar as teclas de acordo com a janela aberta */
 void handleInput(t_tableData *tableData, const int key, unsigned int *currentWindow);
 
 #endif
